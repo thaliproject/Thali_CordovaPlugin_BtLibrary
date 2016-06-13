b40450b5a7e26f36a822e8a4be81f0742aca57aa
Marek Czyz <mczyz@ra.rockwell.com>
2016-06-13 07:58:19 +0200

    Fixed issue #70

--- BtConnectorLib/btconnectorlib2/src/androidTest/java/org/thaliproject/p2p/btconnectorlib/AbstractConnectivityManagerTest.java
+++ BtConnectorLib/btconnectorlib2/src/androidTest/java/org/thaliproject/p2p/btconnectorlib/AbstractConnectivityManagerTest.java
@@ -1,10 +1,12 @@
 package org.thaliproject.p2p.btconnectorlib;
 
 import android.bluetooth.BluetoothAdapter;
+import android.net.wifi.WifiManager;
 import android.support.test.InstrumentationRegistry;
 
 import org.thaliproject.p2p.btconnectorlib.internal.wifi.WifiDirectManager;
 
+import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -20,20 +22,35 @@ public class AbstractConnectivityManagerTest {
             // Device does not support Bluetooth
             fail("No Bluetooth support!");
         }
-        if (turnOn && !btAdapter.isEnabled()) {
+
+        // first make sure bluetooth is not changing its state right now
+        long currentTimeout = 0;
+        while (currentTimeout < MAX_MEDIA_TIMEOUT) {
+            if (btAdapter.getState() == BluetoothAdapter.STATE_ON ||
+                btAdapter.getState() == BluetoothAdapter.STATE_OFF) {
+                break;
+            }
+            Thread.sleep(CHECK_MEDIA_INTERVAL);
+            currentTimeout += CHECK_MEDIA_INTERVAL;
+        }
+        assertThat(btAdapter.getState(), anyOf(is(BluetoothAdapter.STATE_ON),
+                                               is(BluetoothAdapter.STATE_OFF)));
+
+        if (turnOn && btAdapter.getState() == BluetoothAdapter.STATE_OFF) {
             btAdapter.enable();
-        } else if (!turnOn && btAdapter.isEnabled()) {
+        } else if (!turnOn && btAdapter.getState() == BluetoothAdapter.STATE_ON) {
             btAdapter.disable();
         }
 
-        long currentTimeout = 0;
+        int expectedState = turnOn ? BluetoothAdapter.STATE_ON : BluetoothAdapter.STATE_OFF;
+        currentTimeout = 0;
         while (currentTimeout < MAX_MEDIA_TIMEOUT) {
             Thread.sleep(CHECK_MEDIA_INTERVAL);
             currentTimeout += CHECK_MEDIA_INTERVAL;
-            if (btAdapter.isEnabled() != turnOn && currentTimeout < MAX_MEDIA_TIMEOUT) {
+            if (btAdapter.getState() != expectedState && currentTimeout < MAX_MEDIA_TIMEOUT) {
                 continue;
             }
-            assertThat(btAdapter.isEnabled(), is(turnOn));
+            assertThat(btAdapter.getState(), is(expectedState));
             break;
         }
     }
@@ -45,26 +62,42 @@ public class AbstractConnectivityManagerTest {
 
     protected static void toggleWifi(boolean turnOn) throws Exception {
 
-        WifiDirectManager wifiManager = WifiDirectManager.getInstance(InstrumentationRegistry.getContext());
+        WifiManager wifiManager = (WifiManager) InstrumentationRegistry.getContext().getSystemService(
+                InstrumentationRegistry.getContext().WIFI_SERVICE);
 
         if (wifiManager == null) {
             // Device does not support WIFI
             fail("No Wifi support!");
         }
-        if (turnOn && !wifiManager.isWifiEnabled()) {
+
+        // first make sure wifi is not changing its state right now
+        long currentTimeout = 0;
+        while (currentTimeout < MAX_MEDIA_TIMEOUT) {
+            if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED ||
+                    wifiManager.getWifiState() == WifiManager.WIFI_STATE_DISABLED) {
+                break;
+            }
+            Thread.sleep(CHECK_MEDIA_INTERVAL);
+            currentTimeout += CHECK_MEDIA_INTERVAL;
+        }
+        assertThat(wifiManager.getWifiState(), anyOf(is(WifiManager.WIFI_STATE_ENABLED),
+                is(WifiManager.WIFI_STATE_DISABLED)));
+
+        if (turnOn && wifiManager.getWifiState() == WifiManager.WIFI_STATE_DISABLED) {
             wifiManager.setWifiEnabled(true);
-        } else if (!turnOn && wifiManager.isWifiEnabled()) {
+        } else if (!turnOn && wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
             wifiManager.setWifiEnabled(false);
         }
 
-        long currentTimeout = 0;
+        int expectedState = turnOn ? WifiManager.WIFI_STATE_ENABLED : WifiManager.WIFI_STATE_DISABLED;
+        currentTimeout = 0;
         while (currentTimeout < MAX_MEDIA_TIMEOUT) {
             Thread.sleep(CHECK_MEDIA_INTERVAL);
             currentTimeout += CHECK_MEDIA_INTERVAL;
-            if (wifiManager.isWifiEnabled() != turnOn && currentTimeout < MAX_MEDIA_TIMEOUT) {
+            if (wifiManager.getWifiState() != expectedState && currentTimeout < MAX_MEDIA_TIMEOUT) {
                 continue;
             }
-            assertThat(wifiManager.isWifiEnabled(), is(turnOn));
+            assertThat(wifiManager.getWifiState(), is(expectedState));
             break;
         }
     }
